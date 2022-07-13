48a6cf42e63e48dd7f91d6044cec31673108324a
Rohit singh <rohitsingh17031995@gmail.com>
2022-07-13 10:10:32 +0530

    added navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcN7ncYhUbcmxerWlWODn11_g9hScxmqg",
+  authDomain: "facebook-clone-979cc.firebaseapp.com",
+  projectId: "facebook-clone-979cc",
+  storageBucket: "facebook-clone-979cc.appspot.com",
+  messagingSenderId: "442465633576",
+  appId: "1:442465633576:web:0610d81948838b48d03c1d",
+};
